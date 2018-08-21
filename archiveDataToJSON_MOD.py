e91936bdbe364f1ea8d64c00327459c248d8a777
CJuice <CJuice@users.noreply.github.com>
2018-08-21 14:52:52 -0400

    Documenting

--- archiveDataToJSON_MOD.py
+++ archiveDataToJSON_MOD.py
@@ -28,30 +28,35 @@ def main():
     # VARIABLES
     _ROOT_PROJECT_PATH = os.path.dirname(__file__)
     CREDENTIALS_PATH = os.path.join(_ROOT_PROJECT_PATH, "Docs/credentials.cfg")
-    #   Need sensitive information from config file
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser()    # Need sensitive information from config file
     config.read(filenames=CREDENTIALS_PATH)
     USERNAME = config['status_dashboard_archive']["username"]
     PASSWORD = config['status_dashboard_archive']["password"]
-    server_machine_names = {0: config['ags_prod_machine_names']["machine1"],
-                            1: config['ags_prod_machine_names']["machine2"],
-                            2: config['ags_prod_machine_names']["machine3"],
-                            3: config['ags_prod_machine_names']["machine4"]}
     GEODATA_ALIAS = "Geodata Data"
     RESULT_FILE = "GeodataServices.json"
     SERVER_PORT_SECURE = config['ags_prod_machine_names']["secureport"]
     SERVER_ROOT_URL = "https://{machine_name}.mdgov.maryland.gov:{port}"
     SERVER_URL_GENERATE_TOKEN = "arcgis/admin/generateToken"
     SERVER_URL_ADMIN_SERVICES = "arcgis/admin/services"
+    server_machine_names = {0: config['ags_prod_machine_names']["machine1"],
+                            1: config['ags_prod_machine_names']["machine2"],
+                            2: config['ags_prod_machine_names']["machine3"],
+                            3: config['ags_prod_machine_names']["machine4"]}
 
     # CLASSES
     class ItemObject:
-        """TODO"""
+        """Items are services within folders."""
         GEODATA_ROOT = "https://geodata.md.gov/imap/rest/services"
         REST_URL_BEGINNING = "https://{machine_name}.mdgov.maryland.gov:{port}/arcgis/rest/services"
         REST_URL_END = "{folder}/{service_name}/{type}"
 
         def __init__(self, item, folder, machine_name):
+            """
+            Instantiate an ItemObject
+            :param item: json object with information about the service item
+            :param folder: the folder in the services directory where the item is located
+            :param machine_name: the name of the ags server machine currently being interrogated for information
+            """
             self.folder = folder
             self.type = item["type"]
             self.service_name = item["serviceName"]
@@ -66,6 +71,11 @@ def main():
             self.layers = "NA"
 
         def create_base_dictionary(self):
+            """
+            Creates a dictionary of attributes for json.dumps() consumption.
+            This base dictionary is common to all service types.
+            :return:
+            """
             data = {"ServiceName": self.service_name,
                     "Server": GEODATA_ALIAS,
                     "Folder": self.folder,
@@ -88,28 +98,52 @@ def main():
             self.__folder = value.replace("/", "")
 
         @property
-        def rest_service_url_machine(self):
-            return self.__rest_service_url_machine
-
-        @rest_service_url_machine.setter
-        def rest_service_url_machine(self, value):
-            beginning = ItemObject.REST_URL_BEGINNING.format(machine_name=value, port=SERVER_PORT_SECURE)
-            ending = ItemObject.REST_URL_END.format(folder=self.folder, service_name=self.service_name, type=self.type)
-            self.__rest_service_url_machine = f"{beginning}/{ending}"
-
-        @property
         def rest_service_url_geodata(self):
             return self.__rest_service_url_geodata
 
         @rest_service_url_geodata.setter
         def rest_service_url_geodata(self, value):
+            """
+            Creates the url for a service with geodata.md.gov rather than the machine name
+            :param value: Not used, no initial assignment value
+            :return: None
+            """
             ending = ItemObject.REST_URL_END.format(folder=self.folder, service_name=self.service_name, type=self.type)
             self.__rest_service_url_geodata = f"{ItemObject.GEODATA_ROOT}/{ending}"
 
+        @property
+        def rest_service_url_machine(self):
+            return self.__rest_service_url_machine
+
+        @rest_service_url_machine.setter
+        def rest_service_url_machine(self, value):
+            """
+            Creates the url for a service with the machine name for use from a web server to bypass the web adaptor
+            :param value: Initial assignment value is the machine name only
+            :return: None
+            """
+            beginning = ItemObject.REST_URL_BEGINNING.format(machine_name=value, port=SERVER_PORT_SECURE)
+            ending = ItemObject.REST_URL_END.format(folder=self.folder, service_name=self.service_name, type=self.type)
+            self.__rest_service_url_machine = f"{beginning}/{ending}"
+
         def create_json_string(self, data, indent=4):
+            """
+            Create and return a json string from a dictionary.
+            Intended to accept the base dictionary.
+            :param data: a dictionary; designed to accept the base dictionary
+            :param indent: json output pretty indent dimension (default=4)
+            :return: json
+            """
             return json.dumps(obj=data, indent=indent)
 
         def create_json_string_mapserver(self, data, indent=4):
+            """
+            Add an item to a dictionary and then create and return a json string from that dictionary.
+            Intended to accept the base dictionary but to add a key: value pair fit specific to MapServer type services.
+            :param data: a dictionary; designed to accept the base dictionary
+            :param indent: json output pretty indent dimension (default=4)
+            :return: json
+            """
             data["layers"] = self.layers
             return json.dumps(obj=data, indent=indent)
 
@@ -132,7 +166,7 @@ def main():
         def __str__(self):
             """
             Overriding the __str__ builtin to control the appearance of the machine object print-out for readability.
-            :return:
+            :return: string
             """
             return f"{self.machine_name}-->\n\t{self.root_url}\n\t{self.services_url}\n\t{self.token}\n\t{self.folders_list}"
 
@@ -143,6 +177,11 @@ def main():
 
     # FUNCTIONS
     def create_params_for_request(token_action=None):
+        """
+        Create parameters to be submitted with the request.
+        :param token_action: route to be taken when creating the parameters
+        :return: dictionary of parameters
+        """
         if token_action == None:
             values = {'f': 'json'}
         elif token_action == "getToken":
@@ -152,14 +191,21 @@ def main():
         return values
 
     def get_value_from_response(url, params, search_key):
+        """
+        Submit a request with parameters to a url and inspect the response json for the specified key of interest.
+        :param url: url to which to make a request
+        :param params: parameters to accompany the request
+        :param search_key: the key of interest in the response json
+        :return: content of json if key present in response
+        """
         # To deal with mixed path characters between url syntax and os.path.join use of "\"
         url = clean_url_slashes(url)
 
         # FROM ORIGINAL DESIGN HANDLING CLIENT MISMATCH ERROR CONCERNING TOKEN RECOGNITION
         # To deal with the client mismatch error we were encountering, we used the following 'While' to make repeated
         #   requests as a bypass. If the error is no longer encountered because the machine name is now used to bypass
-        #   the web adapter, which seemed to be connected to the issue with token mismatches, the the process will run
-        #   the first time so the 'While' is only run through once and not costing extra time.
+        #   the web adapter, which seemed to be connected to the issue with token mismatches, then the process will run
+        #   the first time so the 'While' is removed but may be of value if similar errors arise in the future.
         # while True:
 
         try:
@@ -173,7 +219,6 @@ def main():
                 if "html" in response.headers["Content-Type"]:
                     raise NotJSONException
                 response_json = response.json()
-                # print(response_json)
             except json.decoder.JSONDecodeError as jde:
                 print("Error decoding response to json: {}".format(jde))
                 print(response)
@@ -188,26 +233,45 @@ def main():
                 value = response_json[search_key]
             except KeyError as ke:
                 print("KeyError: {}".format(ke))
-                # continue # for While loop use
+                # continue      # for While loop usage
                 exit()
             except TypeError as te:
                 print("TypeError: {}".format(te))
                 print(response_json)
-                # continue # for While loop use
+                # continue       # for While loop usage
                 exit()
             else:
                 return value
 
     def clean_url_slashes(url):
+        """
+        Standardize the slashes when use of os.path.join() with forward slashes in url's.
+        os.path.join() uses back slashes '\' while http uses forward slashes '/'
+        :param url: url to be examined
+        :return: standardized url string
+        """
         url = url.replace("\\", "/")
         return url
 
     def create_random_int(upper_integer):
+        """
+        Create and return a random integer from 0 to one less than the upper range value.
+        :param upper_integer: upper integer of range to be used
+        :return: integer
+        """
         options = upper_integer - 1
         spot = random.randint(0, options)
         return spot
 
     def extract_item_properties(item_dict, name_check, extensions="extensions", type_name="typeName"):
+        """
+        Create and return a list of extensions values for a service item based on the service type
+        :param item_dict: item dictionary to be inspected
+        :param name_check: name to check the type against
+        :param extensions: default use was 'extensions' per Jessie's design but could be other values too
+        :param type_name: default use was 'typeName' per Jessie's design but could be other values too
+        :return: list
+        """
         return [entity for entity in item_dict[extensions] if entity[type_name] == name_check]
 
     # FUNCTIONALITY
